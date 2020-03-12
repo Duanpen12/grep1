7c916c7cdc040f3775e7809a34a46f73d6fe0ca5
Ning <duanpen12102535@gmail.com>
2020-03-12 19:05:06 +0100

    add python file test_files.py to github

---
+++ test_files.py
@@ -0,0 +1,31 @@
+def list_from_file(file):
+    open_text = open(file, 'r')
+    content = open_text.readlines()
+    open_text.close()
+    return content
+
+
+text_file_cars = 'cars.txt'
+print_list_from_file = list_from_file(text_file_cars)
+print(print_list_from_file)
+
+
+text_file_people = 'people.txt'
+print_list_from_file_people = list_from_file(text_file_people)
+print(print_list_from_file_people)
+
+
+print_list_from_file.insert(2, 'Duanpen \n')     # how to insert the line at the file
+print(print_list_from_file)
+
+print_list_from_file_people.insert(1, "     \n")
+print(print_list_from_file_people)
+
+
+# how to save file in Python
+print_list_from_file_people.insert(1, "     \n")
+files_to_write = open('new_people.txt', 'w')
+files_to_write.writelines(print_list_from_file_people)
+files_to_write.close()
+print(print_list_from_file_people)
+
